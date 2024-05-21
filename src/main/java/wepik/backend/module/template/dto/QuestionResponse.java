a56adcd7cefdd1d15af04179d590e8575426f545
jschoi-96 <choix712@umn.edu>
2024-05-21 23:24:33 +0900

    feat: 질문 요청, 응답 dto 생성

---
+++ src/main/java/wepik/backend/module/template/dto/QuestionResponse.java
@@ -0,0 +1,36 @@
+package wepik.backend.module.template.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import wepik.backend.module.template.dao.AnswerType;
+import wepik.backend.module.template.dao.Question;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class QuestionResponse {
+
+    @Schema(description = "질문 id", example = "1")
+    private Long id;
+
+    @Schema(description = "질문 제목", example = "질문을 뭐라고 적지?")
+    private String title;
+
+    @Schema(description = "질문 타입", example = "BAR")
+    private AnswerType type;
+
+    @Schema(description = "이미지 URL", example = "https://wepik-static-files4df23447-2355-45h2-8783-7f6gd2ceb848_고양이.jpg")
+    private String imageURL;
+
+
+    public static QuestionResponse fromEntity(Question question) {
+        return QuestionResponse.builder()
+                .id(question.getId())
+                .title(question.getTitle())
+                .type(question.getType())
+                .imageURL(null) // TODO: 파일 만들기 전까지 임시 null
+                .build();
+    }
+}
