d6ab1b10801f61430fef9a5f0ed1ded9ecd2c7a5
jschoi-96 <choix712@umn.edu>
2024-05-22 23:31:57 +0900

    feat: 템플릿 CRUD 기능 구현

--- src/main/java/wepik/backend/module/template/dao/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package wepik.backend.module.template.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-}
