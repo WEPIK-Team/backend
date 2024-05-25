6e9c98cfe649e64a0797461d2337957cbc93a5e4
jschoi-96 <choix712@umn.edu>
2024-05-26 03:30:44 +0900

    refactor: Response 예제 추가

--- src/main/java/wepik/backend/module/file/dto/FileResponse.java
+++ src/main/java/wepik/backend/module/file/dto/FileResponse.java
@@ -15,16 +15,16 @@ public class FileResponse {
     @Schema(description = "파일 id", example = "1")
     private Long id;
 
-    @Schema(description = "사용자가 업로드한 파일명", example = "사진1")
+    @Schema(description = "사용자가 업로드한 파일명", example = "사진")
     private String originalName;
 
-    @Schema(description = "서버에 저장되는 파일명", example = "d23d2dqwt1251asbds사진1")
+    @Schema(description = "서버에 저장되는 파일명", example = "9b30159f-4281-40a3-ae3c-7e850dc132fc_7069124730_486616_30b35cd95fe41e7c1c5c4744bb79eb4f.jpg")
     private String storedName;
 
-    @Schema(description = "업로드한 파일의 경로", example = "/...")
+    @Schema(description = "업로드한 파일의 경로", example = "https://wepik-s3-bucket.s3.ap-northeast-2.amazonaws.com/images/")
     private String path;
 
-    @Schema(description = "업로드한 파일 확장자", example = "jpeg")
+    @Schema(description = "업로드한 파일 확장자", example = "JPEG")
     private FileType type;
 
     @Schema(description = "업도르한 파일 사이즈", example = "236")
