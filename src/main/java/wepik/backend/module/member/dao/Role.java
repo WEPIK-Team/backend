dd21a487109c42efbd3d56dde766096d8b5122f5
jschoi-96 <choix712@umn.edu>
2024-05-25 22:48:15 +0900

    서브모듈 최신화

--- src/main/java/wepik/backend/module/member/dao/Role.java
+++
@@ -1,5 +0,0 @@
-package wepik.backend.module.member.dao;
-
-public enum Role {
-    USER, ADMIN
-}
--- src/main/java/wepik/backend/module/question/dao/AnswerType.java
+++
@@ -1,5 +0,0 @@
-package wepik.backend.module.question.dao;
-
-public enum AnswerType {
-    INPUT, TEXTAREA, SELECT, BAR, STAR_RANK, DATE
-}
