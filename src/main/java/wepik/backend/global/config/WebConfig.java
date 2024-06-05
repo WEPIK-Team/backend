966ccda528a8b587ecc505a9d2c2fc32cc687066
Junsoo Choi <78118588+jschoi-96@users.noreply.github.com>
2024-06-05 19:46:27 +0900

    Fix: localhost:3000, weplik 도메인 cors 허용 (#45)
    Co-authored-by: KangDeok <rkdejr2321@naver.com>

---
+++ src/main/java/wepik/backend/global/config/WebConfig.java
@@ -0,0 +1,26 @@
+package wepik.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://wepik.co.kr") // 요청을 허용하는 도메인
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true)
+                        .allowedHeaders("*")
+                        .maxAge(3600);
+            }
+        };
+    }
+}
