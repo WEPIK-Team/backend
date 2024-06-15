2873ed77f639a90e0ac05dec9f364be82a0039a5
KangDeok <rkdejr2321@naver.com>
2024-06-15 22:17:38 +0900

    Refactor: 암호화 된 패스워드로 관리자 로그인

--- src/main/java/wepik/backend/module/member/application/MemberService.java
+++ src/main/java/wepik/backend/module/member/application/MemberService.java
@@ -3,6 +3,7 @@ package wepik.backend.module.member.application;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import wepik.backend.global.exception.ErrorCode;
 import wepik.backend.global.exception.WepikException;
@@ -19,10 +20,12 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     public boolean login(LoginRequest loginRequest) {
         Member member = memberRepository.findMemberByEmailAndRole(loginRequest.getEmail(), Role.ADMIN)
                 .orElseThrow(() -> new WepikException(ErrorCode.NOT_FOUND_EMAIL));
-        return member.getPassword().equals(loginRequest.getPassword());
+        return passwordEncoder.matches(loginRequest.getPassword(), member.getPassword());
     }
 
 }
---
+++ src/test/java/wepik/backend/module/member/application/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package wepik.backend.module.member.application;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import wepik.backend.module.member.dto.LoginRequest;
+
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void adminPwdEncoding() throws Exception {
+        //given
+        String password = "12345678";
+
+        //when
+        String encoded = passwordEncoder.encode(password);
+
+        //then
+        System.out.println("Encoded password: " + encoded);
+
+    }
+
+    @Test
+    public void adminLoginTest() throws Exception {
+        //given
+        LoginRequest loginRequest = new LoginRequest("wepik@gmail.com", "12345678");
+
+        //when
+        boolean isLogin = memberService.login(loginRequest);
+
+        //then
+        Assertions.assertThat(isLogin).isTrue();
+    }
+
+
+}
